334702e701e0f751f72917129e62bed7ab5b670f
rexflores <rexdaivid.flores@tup.edu.ph>
2024-11-17 01:28:43 +0800

    modified:   app.py

--- app.py
+++ app.py
@@ -68,6 +68,5 @@ def get_emails():
     
     return jsonify(received_emails)
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
-
+if __name__ == '__main__':
+    app.run(debug=True)
